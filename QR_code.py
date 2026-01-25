195dfb6a0cd9bbdf0837eacacba83510b942f337
Om-Shri <som667075@gmail.com>
2026-01-25 07:43:30 +0530

    fix the code

--- QR_code.py
+++ QR_code.py
@@ -1,10 +1,10 @@
 import qrcode
-data = input("Enter url :").strip()
-file_name = input("enter file name:").strip()
+from PIL import Image
+
+data = input("Enter url here :")
+file_name = input("enter file name:")
 
 qr = qrcode.QRCode(box_size=10,border=4)
-
 qr.add_data(data)
 image = qr.make_image(fill_color="black", back_color="white")
-
 image.save(file_name)
