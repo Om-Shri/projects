5ac80e0a536459869c0ec499809b8786a9fc21ff
Om-Shri <som667075@gmail.com>
2026-01-27 18:41:50 +0530

    added some projects

---
+++ TypingSpeed.py
@@ -0,0 +1,33 @@
+from time import *
+import random as r
+
+def mistake(peretest,usertest):
+    error = 0
+    for i in range(len(peretest)):
+        try:
+            if peretest[i] != usertest[i]:
+                error += 1
+        except:
+            error += 1
+    return error
+
+def speed_time(time_s,time_e,userinput):
+    time_taken = round(time_e - time_s,2)
+    words_len = len(userinput)
+    speed = round(words_len/time_taken)
+    return speed
+
+
+test = ["Our life is also a story in which we face many ups and downs and at certain.",
+"He saw that Ant and begged for food, but she said, When I worked hard, you enjoyed."]
+
+print("********SPEED TEST*********","\n")
+
+paragraph = r.choice(test)
+print(paragraph,"\n")
+time1 = time()
+typed = input("**STARTED** \n")
+time2 = time()
+
+print(f"speed : {speed_time(time1,time2,typed)}w/sec")
+print(f"Mistakes : {mistake(paragraph,typed)}")
---
+++ automatic_whatsapp_message.py
@@ -0,0 +1,4 @@
+import pywhatkit as kit
+
+kit.pywhatkit.sendwhatmsg("+910123456789",   "Hello",      12,      12,        5)
+#                        (number to send)   (message)    (hour)  (minute)    (wait) 
